b22d9bb66483ce8ee8900f52869f7032799267c7
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2025-01-26 23:28:47 +0530

    everything done

---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const accountId=12345    // const uses when the value of the variable is not changed in the future
+let accountEmail="harshdeep@gmail.com"  // can be change in the future
+var accountPassword="4321" // it contains the scope problem so we use the let keyword as a variable
+
+accountCity="Etawah"
+
+accountEmail="guptaji@gmail.com"
+accountPassword="111111"
+accountCity="Mysore"
+
+console.table([accountId, accountEmail, accountPassword,accountCity]) // for printing all the variables in one go
+
