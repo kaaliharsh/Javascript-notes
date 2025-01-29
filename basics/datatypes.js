f74f8c298c0db9df2a1fc28075e73210fbdf97d4
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2025-01-30 02:05:07 +0530

    types of datatypes done

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -2,5 +2,21 @@
 
 // alert(3+3)    we are using nodejs not browser
 
+let name="harsh"
+let age=18
+let string="harsh"
+
+let isTrue = true;
+//null=> standalone value
+//undefined=> variable deaclared but not given the value
+//symbol => when talk abou the unique 
+
+//object
+
+console.table([name,age,string,isTrue])
+console.log(typeof age);
+console.log(typeof undefined);  //op--> undefined cuz it is an object in js
+console.log(typeof Object);  //op----> return the function
+
 
 
