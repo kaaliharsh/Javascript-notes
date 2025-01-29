9a54e2f79991b4a6f414e158f0aa50cb16a2091f
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2025-01-30 02:17:34 +0530

    conversion of datatypes

---
+++ basics/conversion_variables.js
@@ -0,0 +1,17 @@
+let score="33a"
+
+console.log(typeof score);
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+
+
+//"33aa"=>  NaN means not a number
+
