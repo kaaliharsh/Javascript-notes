77ba605210456c59f299dbe65c2e50ce5a93d90d
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2025-02-03 19:11:53 +0530

    operations done

---
+++ basics/operations.js
@@ -0,0 +1,27 @@
+let value=3;
+let negvalue=-value
+console.log(negvalue);
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);  //power
+console.log(2/2);
+console.log(2%2);  //modulo
+
+
+let str1="hey"
+let str2=" harsh"
+console.log(str1+str2);
+
+console.log("1"+2+2); // output---- 122 ( js treat the first variable as string then complete operation should be string) 
+console.log(1+2+"2"); //output--- 32  ( if the first varibale is the addition performing then the string is added at the end)
+
+
+
+
+
+
+
+
