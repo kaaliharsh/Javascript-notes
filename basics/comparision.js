ed002ca004ae959be19dcf92190a7781e9ab25f9
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2025-02-03 20:05:50 +0530

    comparision done

---
+++ basics/comparision.js
@@ -0,0 +1,12 @@
+console.log (2 > 1);
+console. log (2 >= 1);
+console.log(2 < 1);
+console. log (2 == 1);
+console.log (2 != 1);
+
+console. log("2" > 1); 
+console. log ("02" > 1);
+
+// === (double equal)   strict check
+
+console.log("2"===2);  // checks the datatypes  
