3b56a61f505757025122a46524c012e13a16c866
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2025-02-03 20:05:01 +0530

    stack and heap memory done

---
+++ basics/memory.js
@@ -0,0 +1,35 @@
+ // +++++++++++++++++++++++++++++++++++++++   Stack and Heap Memory +++++++++++++++++++++++++++++++++++++
+ 
+
+ // primitve datatypes uses the Stack Memory and Reference datatypes uses the Heap memory
+
+
+ let myutubename="btechwala"
+ let anotheutube=myutubename
+
+ anotheutube="no more utube channel"
+ console.log(myutubename);
+ console.log(anotheutube);
+
+
+ // Stack retunrs the copy of the data.
+ // heap returns the reference of the data.
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
