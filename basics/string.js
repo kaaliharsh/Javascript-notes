6fe86172c3a9157999e2fd80a5140504aa78ab51
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2025-02-03 21:10:16 +0530

    strings functions done

---
+++ basics/string.js
@@ -0,0 +1,29 @@
+const str="harsh"
+const repoCount=32
+
+// console.log(str+repoCount+" java is fun");    not much reliable
+
+console.log(`My name is ${str} and my repo count is ${repoCount}.`);  // known as string interpolation
+
+console.log(str[2]);  // return r
+console.log(str.length);
+console.log(str.toLowerCase());
+
+console.log(str.charAt(3));
+console.log(str.indexOf('h'));
+
+const newStr=str.slice(1,3);  // last value not include here that is 3
+console.log(newStr);
+
+trim()
+replace()
+includes()
+substring()
+split(' ')
+charAt()
+
+
+
+
+
+
