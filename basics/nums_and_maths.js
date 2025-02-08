dab5a263366c814c18a4e2afbec5664c56a2ce4a
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2025-02-09 03:02:32 +0530

    numbers done and maths done

--- basics/nums_and_maths.js
+++ basics/nums_and_maths.js
@@ -1,3 +1,24 @@
 const score=400
-const balance=new Number(200);
+const balance=new Number(200); // returns the exact the number type variable
 console.log(balance);
+console.log(balance.toFixed(2)); // used to give the points values
+
+const newNumber=23.456;
+console.log(newNumber.toPrecision(3));   // returns the precise value of the variable or roundoff
+
+// -----------------------------------Maths----------------------------------
+
+
+//console.log(Math);
+
+console.log(Math.abs(-4));     // convert the value into the absolute or positive value
+console.log(Math.random().toFixed(2)*10);   // returns the random value 
+
+
+
+
+
+
+
+
+
