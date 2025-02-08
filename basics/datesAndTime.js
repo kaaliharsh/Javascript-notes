4a72b1ba89965bbf09861c97db00c5211fed84c3
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2025-02-09 03:27:19 +0530

    date and time done

---
+++ basics/datesAndTime.js
@@ -0,0 +1,21 @@
+/// Dates
+
+let date=new Date(); // Date() mathod in js
+console.log(date);
+
+console.log(date.toString());
+console.log(date.toJSON());
+console.log(date.toDateString());   // almost returns the clear date
+
+console.log(typeof date);  // Date is the object in js
+
+let timeStamp=Date.now();
+console.log(timeStamp);  // return the milli seconds
+
+
+
+
+
+
+
+
