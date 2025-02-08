62d6601e36626d329feec337408a54f3c109ed54
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2025-02-09 03:14:43 +0530

    numbers done and maths done

--- basics/nums_and_maths.js
+++ basics/nums_and_maths.js
@@ -14,6 +14,14 @@ console.log(newNumber.toPrecision(3));   // returns the precise value of the var
 console.log(Math.abs(-4));     // convert the value into the absolute or positive value
 console.log(Math.random().toFixed(2)*10);   // returns the random value 
 
+console.log(Math.floor(Math.random()*10));    // floor returns the single value
+
+
+
+
+
+
+
 
 
 
