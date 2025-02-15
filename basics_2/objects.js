e9314c3f618073f24efd03223de8378c3050897e
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2025-02-16 03:33:48 +0530

    objects done

---
+++ basics_2/objects.js
@@ -0,0 +1,6 @@
+// singleton --> Class is defined in such a way that only one instance of the class is created in the complete execution of a program or project. 
+
+// object literals
+
+// Object.create   // through constructor method
+const user={}
