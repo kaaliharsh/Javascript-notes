698e3b2695c685fa80c698ae45e9ebd3799916ae
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2025-02-15 22:25:07 +0530

    array indexing done

--- basics_2/array.js
+++ basics_2/array.js
@@ -1,9 +1,13 @@
 //   Array is a collection of diiferent elements in a single variable.
 // defines with []   long bracket
+// javascript arrays copy operation creates the shallow copies. shallow means it directly changes into the original array value
 
 const myArr=[1,2,3,4,5]   // javascript arrays are resizable and mix of datatypes
 
 console.log(myArr[2]);   // returns the index value of 2 i.e 3
+
+
+
 
 
 
