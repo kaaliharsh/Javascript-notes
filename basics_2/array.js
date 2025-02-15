0e2befda2ceefbeaa098f3581aa9113a6b6c00ea
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2025-02-15 22:22:15 +0530

    array indexing done

---
+++ basics_2/array.js
@@ -0,0 +1,15 @@
+//   Array is a collection of diiferent elements in a single variable.
+// defines with []   long bracket
+
+const myArr=[1,2,3,4,5]   // javascript arrays are resizable and mix of datatypes
+
+console.log(myArr[2]);   // returns the index value of 2 i.e 3
+
+
+
+
+
+
+
+
+
