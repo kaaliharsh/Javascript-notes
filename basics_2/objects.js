4bc1d1a486c610e2fdc4212fbf0812fb613e5708
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2025-02-17 21:03:47 +0530

    objects done

--- basics_2/objects.js
+++ basics_2/objects.js
@@ -3,4 +3,31 @@
 // object literals
 
 // Object.create   // through constructor method
-const user={}
+
+// declare the symbol
+
+const mySymb=Symbol("key1")
+const user={
+    name:"Harsh Deep",
+    [mySymb]:"myKey1", 
+    age:22,
+    location:"Greater Noida",
+    email:"harsh@gmail.com",
+    isLoggedIn:false,
+    lastLogin:["Monday","Saturday"]
+}
+console.log(user.name);
+console.log(user["age"]);
+console.log( user[mySymb]);
+
+// to freeze or unchange the object we use the freeze keywprd
+
+Object.freeze(user)
+user.email="deep@gmail.com"
+console.log(user);
+
+
+
+
+
+
