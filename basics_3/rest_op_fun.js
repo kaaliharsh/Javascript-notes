0b84a9768a7125e964c5d0bbbdfb885a4f303444
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2025-04-29 04:27:09 +0530

    rest op by usng the object

--- basics_3/rest_op_fun.js
+++ basics_3/rest_op_fun.js
@@ -1,4 +1,21 @@
 function calculateTotal(...num){    // ... => rest operator, collect all remaining arguments of a function or elements of an array into a single array
  return num;
 }
-console.log(calculateTotal(100,200,300));  // returns the complete value of numbers
+//console.log(calculateTotal(100,200,300));  // returns the complete value of numbers
+
+
+
+// understanding rest by using the objects concepts
+
+const user={
+    username:"harsh",
+    price:988
+
+}
+
+function handleObject(anyobject){
+    console.log(`user name is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+handleObject(user)
