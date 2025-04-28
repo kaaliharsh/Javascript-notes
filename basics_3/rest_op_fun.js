a014ac2831bdbc9261392ff39dc308381ef0839b
Harsh Deep Gupta <108045259+kaaliharsh@users.noreply.github.com>
2025-04-29 04:21:22 +0530

    rest op

---
+++ basics_3/rest_op_fun.js
@@ -0,0 +1,4 @@
+function calculateTotal(...num){    // ... => rest operator, collect all remaining arguments of a function or elements of an array into a single array
+ return num;
+}
+console.log(calculateTotal(100,200,300));  // returns the complete value of numbers
